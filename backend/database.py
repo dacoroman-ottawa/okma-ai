e56bf80cb5fa2b89c011d7bd2d7c12ea0d0a7fdd
dacoroman-ottawa <62803075+dacoroman-ottawa@users.noreply.github.com>
2026-02-16 14:21:53 -0500

    feat: Switch database connection from SQLite to PostgreSQL.

--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
 
-# Using SQLite for development simplicity (Can be switched to PostgreSQL later)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./kanata_academy.db"
+# Switch to PostgreSQL for development
+# Using current user name (macOS default for Homebrew Postgres)
+SQLALCHEMY_DATABASE_URL = "postgresql://@localhost/kanata_academy"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
